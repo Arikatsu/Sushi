24b60ea4199809281d8124c4790b721c8d0febe9
Scald <104459145+Arikatsu@users.noreply.github.com>
2022-05-31 17:53:38 +0530

    Update ppRate.js

--- commands/fun/ppRate.js
+++ commands/fun/ppRate.js
@@ -1,16 +1,17 @@
 const discord = require("discord.js");
 
 module.exports = {
-    name: "wr",
-    aliases: "waifurate",
+    name: "pp",
+    aliases: "pprate",
     category: "fun",
-    description: "Rates you as a waifu",
+    description: "Rates you your pp size",
     run: async (client, message, args) => {
        if(message.author.bot) return;
+       else if(message.author.id == '593787701409611776') message.channel.send("your dick score is 100/100");
        else
        {
           const rnd = Math.floor(Math.random() * 100)
-          message.channel.send("you are a "+ rnd +"/100 waifu");
+          message.channel.send("your dick score is "+ rnd +"/100");
        }
     }
 }
