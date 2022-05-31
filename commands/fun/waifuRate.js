135e0730e93dc3fb19264b1535a9a4d6abea2f1e
Scald <104459145+Arikatsu@users.noreply.github.com>
2022-05-31 17:54:07 +0530

    Update waifuRate.js

--- commands/fun/waifuRate.js
+++ commands/fun/waifuRate.js
@@ -6,13 +6,12 @@ module.exports = {
     category: "fun",
     description: "Rates you as a waifu",
     run: async (client, message, args) => {
-        if(message.author.bot) return;
-        else if(message.author.id == 593787701409611776)
-          message.channel.send("your dick rating is 100/100"); // I have to be the best smh
-        else
-        {
+       if(message.author.bot) return;
+       else if(message.author.id == '775042627837100033') message.channel.send("you are a 100/100 waifu");
+       else
+       {
           const rnd = Math.floor(Math.random() * 100)
-          message.channel.send("your dick rating is "+ rnd +"/100");
-        }
+          message.channel.send("you are a "+ rnd +"/100 waifu");
+       }
     }
 }
